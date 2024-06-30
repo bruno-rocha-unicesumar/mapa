897689e440a78eda5fa982bc577458e85e5b4cc6
Bruno F. Franco da Rocha <ra-23419081-5@alunos.unicesumar.edu.br>
2024-06-30 18:24:25 -0300

    Adição de novo texto para nova branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Olá, Mundo!");
+        System.out.println("Agora adicionada nova branch atividade-mapa");
     }
 
 }
